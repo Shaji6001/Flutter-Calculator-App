d893a1e0be8c15a17e8a08a9a8809ff9249ee21d
Shaji6001 <shajishaijigmail.com>
2020-11-13 17:58:28 +0300

    Stateful Widget added

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,91 @@
+
 import 'package:flutter/material.dart';
 
 void main()=>runApp(Calculator());
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
   TextEditingController getVal1=TextEditingController();
   TextEditingController getVal2=TextEditingController();
+  TextEditingController getRes=TextEditingController();
+  int result=0;
+  double results=0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Calculator"),backgroundColor: Colors.pinkAccent,
+     home: Scaffold(
+       appBar: AppBar(
+         title: Text("Calculator"),backgroundColor: Colors.lightGreen,
+       ),body: Container(padding: EdgeInsets.all(15.0),
+       child: SingleChildScrollView(
+         child: Column(mainAxisAlignment: MainAxisAlignment.center,
+           crossAxisAlignment: CrossAxisAlignment.center,
+           children: [
+             TextField(controller: getVal1,
+               style: TextStyle(fontSize: 30.0),
+               decoration: InputDecoration(
+                 hintText: "First Value",border: OutlineInputBorder(),
 
-        ),body: Container(padding: EdgeInsets.all(10.0),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextField(controller: getVal1,
-              decoration: InputDecoration(
-                hintText: "Enter Fisrt Value",border: OutlineInputBorder()
-              ),
-            ),SizedBox(height: 10.0,width: 10.0,),
-            TextField(controller: getVal2,
-              decoration: InputDecoration(
-                hintText: "Enter Second Value",border: OutlineInputBorder()
-              ),
-            ),SizedBox(height: 10.0,),
-            RaisedButton(onPressed: (){
-              int getval1=int.parse(getVal1.text);
-              int getval2=int.parse(getVal2.text);
-              int result=getval1+getval2;
-              print(result);
+               ),
+             ),SizedBox(height: 10.0,width: 10.0,),
+             TextField(controller: getVal2,
+               style: TextStyle(fontSize: 30.0),decoration:
+               InputDecoration(
+                 hintText: "Second Value",border: OutlineInputBorder()
+               ),
 
-            },textColor: Colors.pink,child: Text("ADD"),),SizedBox(height: 10.0,),
-            RaisedButton(onPressed: (){
-              int getval1=int.parse(getVal1.text);
-              int getval2=int.parse(getVal2.text);
-              int result=getval1-getval2;
-              print(result);
+             ),SizedBox(height: 15.0,),
 
-            },textColor: Colors.black54,
-            child: Text("SUBSTRACT"),color: Colors.deepPurpleAccent,),
-            RaisedButton(onPressed: (){
-              int getval1=int.parse(getVal1.text);
-              int getval2=int.parse(getVal2.text);
-              int result=getval1*getval2;
-              print(result);
+             RaisedButton(onPressed: (){
+               int getval=int.parse(getVal1.text);
+               int getval2=int.parse(getVal2.text);
+setState(() {
+   result=getval+getval2;
+});
 
-            },child: Text("MULTIPLY"),color: Colors.lightGreen,),
-            RaisedButton(onPressed: (){
-              int getval1=int.parse(getVal1.text);
-              int getval2=int.parse(getVal2.text);
-              double  result=getval1/getval2;
-              print(result);
+               print(result);
+             },
+             color: Colors.deepPurple,child: Text("ADD"),splashColor: Colors.red,),
+             RaisedButton(onPressed: (){
+               int getval1=int.parse(getVal1.text);
+               int getval2=int.parse(getVal2.text);
+   setState(() {
+     result=getval1-getval2;
+   });
+               print(result);
+             },color: Colors.amber,splashColor: Colors.red,
+             child: Text("SUBTRACT"),),
 
-            },child: Text("DIVISION"),color: Colors.purple,)
-          ],
-        ),
-      ),
-      ),
+             RaisedButton(onPressed: (){
+               int getval1=int.parse(getVal1.text);
+               int getval2=int.parse(getVal2.text);
+               setState(() {
+                result=getval1*getval2;
+               });
+               print(result);
+
+             },child: Text("MULTIPLY"),color: Colors.lightGreen,splashColor:
+               Colors.deepPurple,),
+             Text(result.toString()),
+             RaisedButton(onPressed: (){
+               int getval1=int.parse(getVal1.text);
+               int getval2=int.parse(getVal2.text);
+               setState(() {
+                  double result=getval1/getval2;
+               });
+               print(result);
+
+             },child: Text("DIVISION"),color: Colors.pink,splashColor: Colors.deepOrangeAccent,),
+             Text(result.toString()),
+
+           ],
+         ),
+       ),
+     ),
+     ),
     );
   }
 }
-
